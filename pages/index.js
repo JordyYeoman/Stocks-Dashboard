5be2274ab8da0dba3792d2443c95afdaba5a6b3f
Jordy Yeoman <Jordyyeoman@gmail.com>
2020-08-30 11:36:13 +0800

    Mobile Stylinh

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,14 @@ export default () => {
 
   return (
     <div>
-      <div className="p-4 bg-white rounded shadow max-w-md sm:max-w-xl md:max-w-4xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
+      <div className="p-4 bg-white rounded shadow max-w-md sm:max-w-xl md:max-w-4xl lg:max-w-6xl mx-auto lg:px-12 py-8">
         <h1 className="text-2xl font-bold">Stocks</h1>
         <p className="text-gray-500">
           Data stored with Google Sheets + Collected with Cheerio + NodeFetch
           Web scraping
         </p>
       </div>
-      <div className="max-w-md sm:max-w-xl md:max-w-4xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
+      <div className="px-0 sm:px-4 max-w-md sm:max-w-xl md:max-w-4xl lg:max-w-6xl mx-auto md:px-8 lg:px-12 py-8">
         <div className="border-b-2 border-white border-solid py-2 mb-12">
           <h2 className="text-xl text-gray-900">Latest Data</h2>
           <p className="text-white">
